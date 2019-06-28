af3848e04bbb0625ece1ccd189d98e56cfff65b8
flavie-s <52235252+flavie-s@users.noreply.github.com>
2019-06-28 04:14:17 -0500

    Add files via upload

--- export.php
+++ export.php
@@ -3,51 +3,36 @@ session_start ();
 $username = $_SESSION['login'];
 $password = $_SESSION['password'];
 ?>
-<!DOCTYPE html>
-<html>
-<title>Export</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<body class="w3-content" style="max-width:1300px">
-
-<!-- First Grid: Logo & About -->
-<div class="w3-row">
-  <div class="w3-third w3-black w3-container w3-center">
-    <div class="w3-padding-64">
-      <h1>Export</h1>
-    </div>
-  </div>
-  <div class="w3-three-quarters w3-blue-grey w3-container w3-center" style="height:700px">
-    <div class="w3-center">
 <?php
+    
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-      $servername = "localhost";
- fputcsv (resource $handle, array $fields [, string $delimiter = "," [, string $enclosure = '"' [, string $escape_char = "\\" ]]]) : int
+$servername = "localhost";
 
-      try {
-        $conn = new PDO('mysql:host=localhost;dbname=projet', $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	$stmt = $conn->prepare("SELECT REFERENCE, MARQUE, QUANTITE, PRIX, DATE_ACHAT FROM eau");
-    	$stmt->execute();
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=projet', $username, $password);
+} catch (PDOException $e) {
+    // error handler
+}
 
-    	// set the resulting array to associative
-    	$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        print_r($result);
-        $handle=fopen("/var/www/html/projetM2I/Saves/export.csv", "w");
-        foreach($result as $fields);
-            fputcsv($handle, $fields);
-        endforeach;
-        fclose($handle);
-    	}
+header('Content-type: text/csv');
+header('Content-Disposition: attachment; filename="filename.csv"');
 
-	  catch(PDOException $e) {
-    	echo "Error: " . $e->getMessage();
-	  }
+try {
+    $stmt = $pdo->prepare("SELECT * FROM eau");
+    $stmt->execute();
+    $output = fopen('php://output', 'w');
+    $header = true;
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        if ($header) {
+            fputcsv($output, array_keys($row));
+            $header = false;
+        }
+        fputcsv($output, $row);
+    }
+    fclose($output);
+} catch (PDOException $e) {
+    // error handler
+}
 ?>
-    </div>
-  </div>
-</div>
-
-</body>
-</html>
